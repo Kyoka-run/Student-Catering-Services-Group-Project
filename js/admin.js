2654e0cfc9ea130f11d2a7dd885945d9044881a9
Kyoka <132744279+Kyoka-run@users.noreply.github.com>
2024-04-14 04:36:36 +0100

    Add files via upload

--- js/admin.js
+++ js/admin.js
@@ -1,15 +1,18 @@
+// Event listener for the admin login form submission
 document.getElementById('admin-login').addEventListener('submit', function(event) {
-    event.preventDefault();
+    event.preventDefault();  // Prevent the default form submission behavior
 
+    // Retrieving user inputs from the form
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
+    // Check if the entered credentials match the expected admin credentials
     if (username === "admin" && password === "admin123") {
-      // login successful
-      window.location.href = "admin_dashboard.html";
+        // If credentials are correct, redirect to the admin dashboard
+        window.location.href = "admin_dashboard.html";
     } else {
-      // Error message
-      var errorMessage = document.getElementById('error-message');
-      errorMessage.textContent = "Nom d'utilisateur ou mot de passe incorrect.";
+        // If credentials are incorrect, display an error message
+        var errorMessage = document.getElementById('error-message');
+        errorMessage.textContent = "Nom d'utilisateur ou mot de passe incorrect.";
     }
-  });
+});
--- js/register.js
+++ js/register.js
@@ -1,16 +1,19 @@
+// Event listener for handling the submission of the registration form
 document.getElementById('register-form').addEventListener('submit', function(event) {
-    event.preventDefault();
+    event.preventDefault();  // Stops the form from submitting normally
 
+    // Gathering the values entered in the form fields
     var username = document.getElementById('username').value;
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
 
-    // just to check
+    // Logging the information to console for verification
     console.log("Nom d'utilisateur:", username);
     console.log("Adresse e-mail:", email);
     console.log("Mot de passe:", password);
-    // delay to see the console log
+
+    // Redirecting to a dashboard page after a brief delay for seeing the log
     setTimeout(function() {
         window.location.href = "dash.html";
       }, 1000);
-  });
+});
