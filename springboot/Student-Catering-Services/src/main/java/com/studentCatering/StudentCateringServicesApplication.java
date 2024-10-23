76364393323ece0128841db5ddc0d7ec3a2665c9
KyokaKyoka <752572249@qq.com>
2024-10-23 16:05:35 +0100

    Fix file path conflicts

---
+++ springboot/Student-Catering-Services/src/main/java/com/studentCatering/StudentCateringServicesApplication.java
@@ -0,0 +1,13 @@
+package com.studentCatering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCateringServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCateringServicesApplication.class, args);
+	}
+
+}
---
+++ springboot/Student-Catering-Services/src/test/java/com/studentCatering/StudentCateringServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentCatering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCateringServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
